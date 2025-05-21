fc3e379115c81e8f55e385baba5c5e90b776cbc9
theogenejr <inverse.ug@gmail.com>
2025-05-21 10:38:40 +0300

    refactor: remove rewrites configuration from next.config.ts and delete vercel.json

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "/api/:path*",
-      },
-      {
-        source: "/:path*",
-        destination: "/",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
