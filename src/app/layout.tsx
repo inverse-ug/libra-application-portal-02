23152b61211f7891639acc8b70412521c3d1c075
theogenejr <inverse.ug@gmail.com>
2025-05-21 10:28:20 +0300

    refactor: add NextTopLoader component to RootLayout and create vercel.json for rewrites

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { SessionProvider } from "next-auth/react";
+import NextTopLoader from "@/components/next-top-loader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <SessionProvider>
+          <NextTopLoader />
           {children}
           <Toaster richColors position="top-center" />
         </SessionProvider>
